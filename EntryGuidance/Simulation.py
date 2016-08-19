f4573aeee93c19f6343aca9340dabcd7ddd379d1
Connor Noyes <cdnoyes@jpl.nasa.gov>
2016-08-18 22:53:49 -0700

    added a simulation class - no methods written yet.

---
+++ EntryGuidance/Simulation.py
@@ -0,0 +1,39 @@
+
+
+class Simulation:
+
+    def __init__(self):
+    
+    
+    def __dump(self):
+
+    def __call__(self):
+
+    
+    def run(self, InputSample = None):  
+    
+    
+    def plot(self):   
+    
+    
+    def analyze(self):
+       
+    
+    def update(self):
+    
+    
+    def reset(self):
+    
+    
+    def save(self):
+    
+class Cycle: #GuidanceCycle
+    def __init__(self,duration=1,freq=None):
+        if freq is None:
+            self.duration = duration
+            self.rate = 1./duration
+        else:
+            self.duration = 1./rate
+            self.rate = rate
+            
+            
