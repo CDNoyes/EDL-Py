f3ebde6d592a8cc4cbc6ed29c00cb04c40cdf20c
CDNoyes <aero.noyes@gmail.com>
2016-08-19 13:22:02 -0700

    Added a docstring, and added SRP parameters.

--- EntryGuidance/EntryVehicle.py
+++ EntryGuidance/EntryVehicle.py
@@ -1,10 +1,39 @@
 class EntryVehicle:
-    def __init__(self, mass = 2804.0, area = 15.8, CD = 0, CL = 0):
+    '''
+    Defines an EntryVehicle Class:
+    
+    members:
+        mass - the initial mass of the vehicle, kg
+        area - the effective area, m^2
+        CD   - a multiplicative offset for the vehicle's drag coefficient
+        CL   - a multiplicative offset for the vehicle's lift coefficient
+        
+        SRP-related parameters:
+        Thrust - the total thrust of the vehicle BEFORE efficiency losses, cant angle considerations, etc, Newtons
+        ThrustFactor - mean(cos(cantAngles)*(1-superSonicEfficiencyLosses)), non-dimensional
+        Isp - specific impulse, s
+        g0 - sea level Earth gravity, used in mass rate of change, m/s^2
+    
+    methods:
+        mdot(throttle) - computes the mass rate of change based on the current throttle setting
+        aerodynamic_coefficients(Mach) - computes the values of CD and CL for the current Mach values
+        
+    '''
+    
+    def __init__(self, mass = 2804.0, area = 15.8, CD = 0, CL = 0, Thrust = 10375, Isp = 260, ThrustFactor = 1):
         self.mass = mass
         self.area = area
         self.CD = CD
         self.CL = CL
-
+        
+        self.Thrust = Thrust
+        self.ThrustLoss = ThrustLoss
+        self.Isp = Isp
+        self.g0 = 9.81
+        
+        
+    def mdot(throttle):
+        return -self.Thrust*throttle/(self.g0*self.isp)
         
     def aerodynamic_coefficients(self, M):
         pD = [2.598e4, -1022.0, -2904.0, 678.6, -44.33, 1.373]
