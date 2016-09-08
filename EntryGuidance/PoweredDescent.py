d073245e22cd8b98e563402b4a50c2dc69eabd2c
Connor Noyes <cdnoyes@jpl.nasa.gov>
2016-09-07 19:29:06 -0700

    Added thrust angle costate to dynamics to properly compute the Hamiltonian.

--- EntryGuidance/PoweredDescent.py
+++ EntryGuidance/PoweredDescent.py
@@ -43,7 +43,7 @@ def getProblem():
 
     problem['gain'] = 50
 
-    problem['tf'] = 12.28  # An initial guess at the final time
+    problem['tf'] = None  
     return problem
 
 
@@ -58,14 +58,21 @@ def  EoM(X,t, lx,ly,lu0,lv0, problem,output=False):
 
     g = 3.7
 
-    x, y, u, v, m, mu, lm = X
+    x, y, u, v, m, mu, lm, lmu = X
     
     lu = lu0-lx*t
     lv = lv0-ly*t
 
+    # if np.abs(lmu) <= 1:
+        # target = np.pi+np.arctan2(lv, lu)
+        # dmu = sat(problem['gain']*(target-mu), -problem['mudotmax'], problem['mudotmax'])
+    # else:
+        # dmu = -problem['mudotmax']*np.sign(lmu)
+        
     tTurn = np.abs(np.pi/2.0-mu)/problem['mudotmax']
-    if (t+tTurn) >= problem['tf']:
+    if (t+tTurn-0.03) >= problem['tf']:
         target = np.pi/2.0
+        # dmu = np.fmin(0,np.sign(target-mu)*problem['mudotmax'])
         dmu = np.sign(target-mu)*problem['mudotmax']
     else:
         target = np.pi+np.arctan2(lv, lu)
@@ -83,34 +90,38 @@ def  EoM(X,t, lx,ly,lu0,lv0, problem,output=False):
     dv = T*sin(mu)/m-g
     dm = -T/problem['ve']
     dlm = T*(lu*cos(mu)+lv*sin(mu))/m**2
-
+    dlmu = T*(lu*sin(mu)-lv*cos(mu))/m
+    
     if output:
         return T, dmu
     else:
-        return np.array([dx, dy, du, dv, dm, dmu, dlm])
+        return np.array([dx, dy, du, dv, dm, dmu, dlm, dlmu])
     
 
 
 def PMPCost(guess,problem):
 
-    lx,ly,lu0,lv0,lm0,tf = guess
+    lx,ly,lu0,lv0,lm0,lmu0,tf = guess
     problem['tf'] = tf
-    x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0])
-    X = odeint(EoM, x0, np.linspace(0,tf,500), args=(lx, ly, lu0, lv0, problem))
+    x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0,lmu0])
+    X = odeint(EoM, x0, np.linspace(0,tf+0.03,500), args=(lx, ly, lu0, lv0, problem))
 
     imax = X.shape[0]
     for i in range(imax):
         if np.all(X[i, :] == 0.0):
+            print "Debug: Stripping Bad Values"
             imax = i
             break
 
     Xf = X[imax-1,:]
-    xf,yf,uf,vf,mf,muf,lmf = Xf
+    xf,yf,uf,vf,mf,muf,lmf,lmuf = Xf
     luf = lu0-lx*tf
     lvf = lv0-ly*tf
-    dx, dy, du, dv, dm, dmu, dlm = EoM(Xf, tf, lx, ly, lu0, lv0, problem, output=False)
+    dx, dy, du, dv, dm, dmu, dlm, dlmu = EoM(Xf, tf, lx, ly, lu0, lv0, problem, output=False)
+    dx0, dy0, du0, dv0, dm0, dmu0, dlm0, dlmu0 = EoM(Xf, tf, lx, ly, lu0, lv0, problem, output=False)
 
-    H = lx*uf + ly*vf + luf*du + lvf*dv + lmf*dm # Ignores the lambda_mu term since we can set the final mu_dot to 0
+    Hf = lx*uf + ly*vf + luf*du + lvf*dv + lmf*dm + lmuf*dmu 
+    H0 = lx*problem['u0'] + ly*problem['v0'] + luf*du0 + lvf*dv0 + lmf*dm0 + lmuf*dmu 
     
     g = [
       # Equality Constraints
@@ -118,8 +129,8 @@ def PMPCost(guess,problem):
       yf-problem['yf'],
       uf-problem['uf'],
       vf-problem['vf'],
-      # H,                      # Free tf, transversality condition
-      muf-problem['muf'],   # Ensures a vertical landing, could be automatically satisfied
+      0*Hf,                      # Free tf, transversality condition
+      0*(muf-problem['muf']),   # Ensures a vertical landing, could be automatically satisfied
       lmf + 1]
 
     return g
@@ -130,16 +141,22 @@ def PMPOpt():
 
     problem = getProblem()
     # guess = [0.0793,0.1265,2.66,-0.3638,-0.755,12.28]
-    guess = [g*1.0 for g in [0.0793,0.1265,2.66,-0.3638,-0.755,12.9]]
+    # guess = [0.0804911135750877, 0.126960707251939, 2.67099166231944, -0.363124805249400,-0.754774141665489,-9.26285943305668,12.263]
+    guess = [0.08042227,   0.12706492,   2.66632126,  -0.36258304,  -0.75506248,  -9.26403713,  12.26226516]
+
+    # guess = [g*1.0 for g in [0.0799,0.1265,2.66,-0.3638,-0.755,-9.305,12.28]]
+    
     sol = root(PMPCost, guess, args=(problem))
     xsol = sol['x']
     print sol['message']
     print "Number of function evaluations: {}".format(sol['nfev'])
-
+    print xsol
     # xsol = guess
+    # problem['tf'] = guess[-1]
+    
+    t, x,y, u,v, udot,vdot, m,T, mu,mudot, lx,ly,lu,lv,lm,lmu, s, H,Hp,Hv,Hm,Hmu = PMPParse(xsol,problem)
 
-    t, x,y, u,v, udot,vdot, m,T, mu,mudot, lx,ly,lu,lv,lm, s, H = PMPParse(xsol,problem)
-
+    mu_dot_exp = sat(-T/(m*problem['ve'])*(lu*cos(mu)+lv*sin(mu))/(lu*sin(mu)-lv*cos(mu)),-problem['mudotmax'],-problem['mudotmax'])
 
     print("Prop used: {} kg".format(m[0]-m[-1]))
 
@@ -171,31 +188,38 @@ def PMPOpt():
     
     plt.figure()
     plt.plot(t,mudot*180/np.pi)
+    plt.plot(t,mu_dot_exp*180/np.pi)
+    plt.legend(('True','Hdot=0'))
     plt.title('Mu dot')
     
     plt.figure()
-    for l in [lx,ly,lu,lv,lm]:
+    for l in [lx,ly,lu,lv,lm,lmu]:
         plt.plot(t,l)
     plt.title('Costates')
 
-    # plt.figure()
+    plt.figure()
     # plt.plot(t,s)
-    # plt.title('Switching fun')
+    plt.plot(t,lu*sin(mu)-lv*cos(mu))
+    plt.title('Switching fun')
 
     plt.figure()
     plt.plot(t,H)
-    plt.title('Hamiltonian (Approx, no lambda mu term)')
+    for h in [Hp,Hv,Hm,Hmu]:
+        plt.plot(t,h)
+    plt.title('Hamiltonian')
+    plt.legend(('H','pos','vel','mass','mu'))
+    
 
     plt.show()    
 
 
 def PMPParse(guess,problem):
-
+    print "Parsing solution"
     g = 3.7
-    lx,ly,lu0,lv0,lm0,tf = guess
+    lx,ly,lu0,lv0,lm0,lmu0,tf = guess
     problem['tf'] = tf
-    x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0])
-    t = np.linspace(0, tf, 500)
+    x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0,lmu0])
+    t = np.linspace(0, tf+0.03, 500)
 
     X = odeint(EoM, x0, t, args = (lx,ly,lu0,lv0,problem))
 
@@ -207,12 +231,12 @@ def PMPParse(guess,problem):
             break
 
     t = t[0:imax]
-    x, y, u, v, m, mu, lm= X[0:imax, 0], X[0:imax, 1], X[0:imax, 2], X[0:imax, 3], X[0:imax, 4], X[0:imax, 5], X[0:imax, 6]
+    x, y, u, v, m, mu, lm, lmu= X[0:imax, 0], X[0:imax, 1], X[0:imax, 2], X[0:imax, 3], X[0:imax, 4], X[0:imax, 5], X[0:imax, 6], X[0:imax, 7]
     
     lu = lu0-lx*t
     lv = lv0-ly*t
-    mu_dot = np.zeros_like(t)
-    T = np.zeros_like(t)
+    mu_dot = np.ones_like(t)
+    T = np.ones_like(t)
     s = (lu*cos(mu)+lv*sin(mu)) - m*lm/problem['ve']
 
     for i in range(len(t)):
@@ -221,10 +245,13 @@ def PMPParse(guess,problem):
     udot = T*cos(mu)/m
     vdot = T*sin(mu)/m-g
 
-    H = lx * u + ly * v + lu * udot + lv * vdot + lm * -T/problem['ve']
+    H = lx * u + ly * v + lu * udot + lv * vdot + lm * -T/problem['ve'] + lmu*mu_dot
+    Hp = lx * u + ly * v
+    Hv = lu * udot + lv * vdot
+    Hm = lm * -T/problem['ve']
+    Hmu = lmu*mu_dot
 
-
-    return t,x,y,u,v,udot,vdot,m,T,mu,mu_dot, lx*np.ones_like(t),ly*np.ones_like(t),lu,lv,lm, s, H
+    return t,x,y,u,v,udot,vdot,m,T,mu,mu_dot, lx*np.ones_like(t),ly*np.ones_like(t),lu,lv,lm,lmu, s, H, Hp,Hv,Hm,Hmu
     
 # ########################### #
 # Polynomial Based Approaches #
