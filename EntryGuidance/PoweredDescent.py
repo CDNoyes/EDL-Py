6fee892486f14a2fb4f1894e4aa47d8a67bdcdf4
Connor Noyes <cdnoyes@jpl.nasa.gov>
2016-09-15 14:02:22 -0700

    Scaled the final time to 1, and began adding homotopy methods and parameters.

--- EntryGuidance/PoweredDescent.py
+++ EntryGuidance/PoweredDescent.py
@@ -41,9 +41,10 @@ def getProblem():
     problem['mu0'] = np.pi+fpa0
     problem['mudotmax'] = 40*np.pi/180  # 40 deg/s
 
-    problem['gain'] = 50
-
+    problem['gain'] = 500
     problem['tf'] = None  
+    
+    # problem['sol0'] = # The solution to the initial problem beginning the homotopy.
     return problem
 
 
@@ -52,22 +53,18 @@ def getProblem():
 # Pontryagin Approach  #
 # #################### #
 
-def  EoM(X,t, lx,ly,lu0,lv0, problem,output=False):
+def  EoM(X,t, lx,ly,lu0,lv0, problem, output=False):
     # Defines the equations of motion for 2-D powered flight and the associated costates.
-    # Only one costate needs to be integrated numerically.
+    # Only two costates needs to be integrated numerically.
 
+    t = t*problem['tf'] # Create the true time
+    
     g = 3.7
 
     x, y, u, v, m, mu, lm, lmu = X
     
     lu = lu0-lx*t
     lv = lv0-ly*t
-
-    # if np.abs(lmu) <= 1:
-        # target = np.pi+np.arctan2(lv, lu)
-        # dmu = sat(problem['gain']*(target-mu), -problem['mudotmax'], problem['mudotmax'])
-    # else:
-        # dmu = -problem['mudotmax']*np.sign(lmu)
         
     tTurn = np.abs(np.pi/2.0-mu)/problem['mudotmax']
     if (t+tTurn-0.03) >= problem['tf']:
@@ -75,8 +72,10 @@ def  EoM(X,t, lx,ly,lu0,lv0, problem,output=False):
         # dmu = np.fmin(0,np.sign(target-mu)*problem['mudotmax'])
         dmu = np.sign(target-mu)*problem['mudotmax']
     else:
+        dmuOpt = (lx*lv/lu**2 - ly/lu)/sec(mu)**2
         target = np.pi+np.arctan2(lv, lu)
-        dmu = sat(problem['gain']*(target-mu), -problem['mudotmax'], problem['mudotmax'])
+        dmuReach = problem['gain']*(target-mu)
+        dmu = sat(dmuReach, -problem['mudotmax'], problem['mudotmax'])
 
     s = (lu*cos(mu)+lv*sin(mu)) - m*lm/problem['ve']
     if s <= 0:
@@ -95,16 +94,27 @@ def  EoM(X,t, lx,ly,lu0,lv0, problem,output=False):
     if output:
         return T, dmu
     else:
-        return np.array([dx, dy, du, dv, dm, dmu, dlm, dlmu])
+        return np.array([dx, dy, du, dv, dm, dmu, dlm, dlmu])*problem['tf']
     
+def EoM_Simple(X,t,problem):
+    return t*np.ones_like(X)
 
+    
+def EoM_Homotopy(X,t, lx,ly,lu0,lv0, problem,h):
+    return (1-h)*EoM_Simple(X,t,problem)+h*EoM(X,t, lx,ly,lu0,lv0, problem)
+    
+    
+def SimpleCost(guess,problem):
+    
+    return 
 
 def PMPCost(guess,problem):
 
     lx,ly,lu0,lv0,lm0,lmu0,tf = guess
     problem['tf'] = tf
     x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0,lmu0])
-    X = odeint(EoM, x0, np.linspace(0,tf+0.03,500), args=(lx, ly, lu0, lv0, problem))
+    # X = odeint(EoM, x0, np.linspace(0,tf+0.03,500), args=(lx, ly, lu0, lv0, problem))
+    X = odeint(EoM, x0, np.linspace(0,1+0.03/tf,500), args=(lx, ly, lu0, lv0, problem))
 
     imax = X.shape[0]
     for i in range(imax):
@@ -129,9 +139,10 @@ def PMPCost(guess,problem):
       yf-problem['yf'],
       uf-problem['uf'],
       vf-problem['vf'],
-      0*Hf,                      # Free tf, transversality condition
       0*(muf-problem['muf']),   # Ensures a vertical landing, could be automatically satisfied
-      lmf + 1]
+      
+      0*Hf,                     # Free tf, transversality condition
+      lmf + 1]                  # Free final mass, so the associated costate is fixed at the final time
 
     return g
 
@@ -144,20 +155,20 @@ def PMPOpt():
     # guess = [0.0804911135750877, 0.126960707251939, 2.67099166231944, -0.363124805249400,-0.754774141665489,-9.26285943305668,12.263]
     guess = [0.08042227,   0.12706492,   2.66632126,  -0.36258304,  -0.75506248,  -9.26403713,  12.26226516]
 
-    # guess = [g*1.0 for g in [0.0799,0.1265,2.66,-0.3638,-0.755,-9.305,12.28]]
+    # guess = [g*1.01 for g in guess]
     
     sol = root(PMPCost, guess, args=(problem))
     xsol = sol['x']
     print sol['message']
-    print "Number of function evaluations: {}".format(sol['nfev'])
+    print "Number of function evaluations: {}\n".format(sol['nfev'])
     print xsol
     # xsol = guess
     # problem['tf'] = guess[-1]
-    
+
     t, x,y, u,v, udot,vdot, m,T, mu,mudot, lx,ly,lu,lv,lm,lmu, s, H,Hp,Hv,Hm,Hmu = PMPParse(xsol,problem)
 
-    mu_dot_exp = sat(-T/(m*problem['ve'])*(lu*cos(mu)+lv*sin(mu))/(lu*sin(mu)-lv*cos(mu)),-problem['mudotmax'],-problem['mudotmax'])
-
+    mu_dot_opt = (lx*lv/lu**2 - ly/lu)/sec(mu)**2
+    
     print("Prop used: {} kg".format(m[0]-m[-1]))
 
     plt.figure()
@@ -188,8 +199,8 @@ def PMPOpt():
     
     plt.figure()
     plt.plot(t,mudot*180/np.pi)
-    plt.plot(t,mu_dot_exp*180/np.pi)
-    plt.legend(('True','Hdot=0'))
+    plt.plot(t,mu_dot_opt*180/np.pi)
+    plt.legend(('Proportional','Formula'))
     plt.title('Mu dot')
     
     plt.figure()
@@ -214,15 +225,15 @@ def PMPOpt():
 
 
 def PMPParse(guess,problem):
-    print "Parsing solution"
+    print "Parsing solution:"
     g = 3.7
     lx,ly,lu0,lv0,lm0,lmu0,tf = guess
     problem['tf'] = tf
     x0 = np.array([problem['x0'],problem['y0'],problem['u0'],problem['v0'],problem['m0'],problem['mu0'],lm0,lmu0])
-    t = np.linspace(0, tf+0.03, 500)
+    tau = np.linspace(0, 1+0.03/tf, 500)
 
-    X = odeint(EoM, x0, t, args = (lx,ly,lu0,lv0,problem))
-
+    X = odeint(EoM, x0, tau, args = (lx,ly,lu0,lv0,problem))
+    t = tau*tf # Turn t into true time after integration is done
     imax = len(t)
     for i in range(len(t)):
         if np.all(X[i, :] == 0.0):
@@ -240,7 +251,7 @@ def PMPParse(guess,problem):
     s = (lu*cos(mu)+lv*sin(mu)) - m*lm/problem['ve']
 
     for i in range(len(t)):
-        T[i], mu_dot[i] = EoM(X[i,:],t[i], lx, ly, lu[0], lv[0], problem, output=True)
+        T[i], mu_dot[i] = EoM(X[i,:],tau[i], lx, ly, lu[0], lv[0], problem, output=True)
 
     udot = T*cos(mu)/m
     vdot = T*sin(mu)/m-g
