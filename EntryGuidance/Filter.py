41f023678a98c1ff9597a8a02897ea7864f5f14c
Connor Noyes <cdnoyes@jpl.nasa.gov>
2016-10-29 13:28:12 -0700

    Removed unneeded imports.

--- EntryGuidance/Filter.py
+++ EntryGuidance/Filter.py
@@ -1,8 +1,5 @@
 ''' Defines filters for estimation '''
 
-from numpy import sin, cos, arctan2
-from numpy.linalg import norm
-import numpy as np
 
 
 def FadingMemory(currentValue, measuredValue, gain):
