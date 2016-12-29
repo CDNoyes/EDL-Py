af93fe544c9fa4c55a466757eb408b6e76501fe5
CDNoyes <aero.noyes@gmail.com>
2016-12-29 14:21:14 -0800

    Switched reconstruction to use an interpolation function for use in numerical differencing. Not sure it changes anything but all the arrays are the same length as a result.

--- EntryGuidance/Drag.py
+++ EntryGuidance/Drag.py
@@ -1,8 +1,11 @@
-from EntryEquations import EDL, Saturate
+""" Reconstruction of families of trajectories defined by a reference profile. """
+
+from EntryEquations import EDL
 
 import numpy as np
 from scipy.optimize import root
 from scipy.integrate import cumtrapz
+from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 
 
@@ -23,7 +26,7 @@ def solve(E, D, m, model):
     r = []
     v = []
     for e,d in zip(E,D): 
-        sol = root(cost, guess, args=(e,d,m,model), tol=1e-5, options={'eps': 1e-6, 'diag':(3e-7,2e-4)})
+        sol = root(cost, guess, args=(e,d,m,model), tol=1e-5, options={'eps': 1e-8, 'diag':(3e-7,2e-4)})
         r.append(sol['x'][0])
         v.append(sol['x'][1])
         guess = sol['x']
@@ -42,29 +45,35 @@ def reconstruct(E,D,h,v,m,model):
     
     r = h*1e3 + model.planet.radius
 
-    if E is None:
+    if E is None:                                   # h-v profile
         E = model.energy(r, v, Normalized=False)
         L,D = model.aeroforces(r,v,m)
-    else:
+    else:                                           # D-E profile
         L = model.aeroforces(r,v,m)[0]
    
     g = model.planet.mu/r**2
 
-    e_dot = -D[:-2]*v[:-2]
+    e_dot = -D*v
     
-    h_prime = np.diff(h*1e3)/np.diff(E)
-    fpa = np.arcsin(np.clip(-h_prime*D[:-1],-1,1))
+    hE = interp1d(E[::-1], h[::-1]*1e3, fill_value=(h[-1]*1e3,h[0]*1e3), assume_sorted=True, bounds_error=False, kind='cubic')
+    dE = 1 # Might be small enough since energy is large
+    h_prime = (hE(E+dE)-hE(E-dE))/2/dE
+    h_prime[0] = h_prime[1]
+    h_prime[-1] = h_prime[-2]
+
+    fpa = np.arcsin(np.clip(-h_prime*D,-1,1))
     
-    s = cumtrapz(-np.cos(fpa)/D[:-1], E[:-1], initial=0)/1000 # True
-    # s = cumtrapz(-1/D, E)/1000  # Approx - turns out the approximation is very good...
+    s = cumtrapz(-np.cos(fpa)/D, E, initial=0)/1000 # True
     t = cumtrapz(-1/D/v, E, initial=0)
     
+    dt = 1e-5
+    fpaT = interp1d(t, fpa, fill_value=(fpa[0],fpa[-1]), assume_sorted=True, bounds_error=False, kind='cubic')
+    gamma_dot = (fpaT(t+dt)-fpaT(t-dt))/2/dt
+    gamma_dot[0]=gamma_dot[1]
+    gamma_dot[-1]=gamma_dot[-2]
     
-    gamma_dot = np.diff(fpa)/np.diff(E[:-1])*e_dot
-    
-    u = (gamma_dot + (g[:-2]/v[:-2]-v[:-2]/r[:-2])*np.cos(fpa[:-1]))*v[:-2]/L[:-2]
-    
-    
+    u = (gamma_dot + (g/v-v/r)*np.cos(fpa))*v/L
+
     return np.degrees(fpa), s, u, t
     
 if __name__ == '__main__':
@@ -73,59 +82,117 @@ if __name__ == '__main__':
     from ParametrizedPlanner import HEPBank,HEPBankSmooth
     from Uncertainty import getUncertainty
     from InitialState import InitialState
+    from MCF import mcsplit, mcfilter
     
     # Plan the nominal profile:
-    reference_sim = Simulation(cycle=Cycle(.1),output=False,**EntrySim())
+    reference_sim = Simulation(cycle=Cycle(0.1),output=False,**EntrySim())
     bankProfile = lambda **d: HEPBankSmooth(d['time'],*[99.67614316,  117.36691891,  146.49573609],minBank=np.radians(30))
                                                 
     x0 = InitialState()
-    output = reference_sim.run(x0,[bankProfile])
-
-    track_drag = 1
-    
+    # output = reference_sim.run(x0,[bankProfile]) 
+    output = reference_sim.run(x0,[bankProfile],[0,-.15,-0.15,0.0]) #Plan with less lift
     references = reference_sim.getRef()
-    if track_drag:
-        istart = np.argmax(output[:,1])
-    else:
-        istart = np.argmax(output[:,7])
-        
-    energy = output[istart:,1]
-    drag = output[istart:,13]/np.cos(np.radians(output[istart:,8]))
-    # lift = output[istart:,12]
-    sstart = output[istart,10] 
-    reference_sim.plot(plotEnergy=True)  
+
+    # track_drag = 1
+    for track_drag in range(2):
     
-    # samples = [np.zeros(4)]
-    # samples = np.array([np.zeros(4), [-.2, -.1, .1, .2], np.zeros(4), np.zeros(4)]) # Lift dispersions
-    # samples = np.array([[-.2, -.1, .1, .2], np.zeros(4), np.zeros(4), np.zeros(4)]) # Drag dispersions
-    
-    samples = getUncertainty()['parametric'].sample(10, 'S')
-    
-    # pdf = getUncertainty()['parametric'].pdf(samples)
-    for sample in samples.T:
         if track_drag:
-            h, v = solve(energy, drag, x0[7], model = EDL(sample))
-        else:    
-            h, v = output[istart:,3], output[istart:,7]
-            energy = None
-        fpa, s, u, t = reconstruct(energy, drag, h, v, x0[7]*np.ones_like(h), model = EDL(sample))
+            # istart = np.argmax(output[:,1])
+            istart = np.where(output[:,13] > 8)[0][0]
+            iv = output[istart:,1]
+        else:
+            istart = np.argmax(output[:,7])
+            iv = output[istart:,7]
+            
+        energy = output[istart:,1]
+        time = output[istart:,0]
+        altitude = output[istart:,3]
+        velocity = output[istart:,7]
+        drag = output[istart:,13] #/np.cos(np.radians(output[istart:,8]))
+        # lift = output[istart:,12]
+        # sstart = output[istart,10] 
+        sstart = (reference_sim.history[0,6]-reference_sim.history[istart,6])/1000 
+        # reference_sim.plot(plotEnergy=True)  
         
-        plt.figure(1)
-        plt.plot(v,h,label="pert: {}".format(sample))
+        # samples = [np.zeros(4)]
+        # samples = np.array([np.zeros(4), [-.2, -.1, .1, .2], np.zeros(4), np.zeros(4)]) # Lift dispersions
+        # samples = np.array([[-.2, -.1, .1, .2], np.zeros(4), np.zeros(4), np.zeros(4)]) # Drag dispersions
         
-        plt.figure(3)
-        plt.plot(v[:-1],s+sstart,label="pert: {}".format(sample))
+        samples = getUncertainty()['parametric'].sample(200, 'S')
         
-        plt.figure(5)
-        plt.plot(v[:-2],u,label="pert: {}".format(sample))        
+        # pdf = getUncertainty()['parametric'].pdf(samples)
+        hdata = []
+        sdata = []
+        fdata = []
+        udata = []
         
-        plt.figure(7)
-        plt.plot(v[:-1],fpa,label="pert: {}".format(sample))
+        for sample in samples.T:
+            if track_drag:
+                h, v = solve(energy, drag, x0[7], model = EDL(sample))
+            else:    
+                h, v = output[istart:,3], output[istart:,7]
+                energy = None
+            fpa, s, u, t = reconstruct(energy, drag, h, v, x0[7]*np.ones_like(h), model = EDL(sample))
+            
+            hdata.append(h)
+            sdata.append(s)
+            fdata.append(fpa)
+            
+            plt.figure(1+track_drag)
+            plt.plot(iv,h,'m',alpha=0.1,label="pert: {}".format(sample))
+            
+            plt.figure(3+track_drag)
+            plt.plot(iv,s+sstart-2.06,'m',alpha=0.1,label="pert: {}".format(sample))
+            
+            plt.figure(5+track_drag)
+            plt.plot(iv,u,'m',alpha=0.1,label="pert: {}".format(sample))        
+            
+            plt.figure(7+track_drag)
+            plt.plot(iv,fpa,'m',alpha=0.1,label="pert: {}".format(sample))
+            
+
+        sdata = np.array(sdata).T + sstart - 2.06
+        s_mean = np.mean(sdata, axis=1)
+        s_var = np.diag(np.cov(sdata))
+        s_std = np.sqrt(s_var)
+        s_upper = s_mean + 3*s_std
+        s_lower = s_mean - 3*s_std
         
-    plt.legend(loc='best')
+        hdata = np.array(hdata).T
+        h_mean = np.mean(hdata, axis=1)
+        h_var = np.diag(np.cov(hdata))
+        h_std = np.sqrt(h_var)
+        h_upper = h_mean + 3*h_std
+        h_lower = h_mean - 3*h_std
+        
+        fdata = np.array(fdata).T
+        f_mean = np.mean(fdata, axis=1)
+        f_var = np.diag(np.cov(fdata))
+        f_std = np.sqrt(f_var)
+        f_upper = f_mean + 3*f_std
+        f_lower = f_mean - 3*f_std
+        
+        plt.figure(1+track_drag)
+        plt.plot(iv, h_upper, 'k--',lw = 3)
+        plt.plot(iv, h_lower, 'k--',lw = 3)
+        plt.plot(iv, h_mean, 'k',lw = 3)
+        
+        plt.figure(3+track_drag)
+        plt.plot(iv, s_upper, 'k--',lw = 3)
+        plt.plot(iv, s_lower, 'k--',lw = 3)
+        plt.plot(iv, s_mean, 'k',lw = 3)
+        
+        plt.figure(7+track_drag)
+        plt.plot(iv, f_upper, 'k--',lw = 3)
+        plt.plot(iv, f_lower, 'k--',lw = 3)
+        plt.plot(iv, f_mean, 'k',lw = 3)
+    
+    # mcsplit(samples, hdata, criteria)
+    
+    # plt.legend(loc='best')
     plt.show()
     
-    import chaospy as cp
-    polynomials = cp.orth_ttr(order=5, dist=getUncertainty()['parametric']) 
-    nodes, weights = cp.generate_quadrature(order=5, domain=getUncertainty()['parametric'], rule="Gaussian")
-    print nodes.shape
+    # import chaospy as cp
+    # polynomials = cp.orth_ttr(order=5, dist=getUncertainty()['parametric']) 
+    # nodes, weights = cp.generate_quadrature(order=5, domain=getUncertainty()['parametric'], rule="Gaussian")
+    # print nodes.shape
