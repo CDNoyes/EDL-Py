784f8b314450b1910a4f9be244304b2aeac1a3d5
CDNoyes <aero.noyes@gmail.com>
2017-01-04 21:36:14 -0800

    Added setup to compute individual variance contributions.

--- EntryGuidance/Drag.py
+++ EntryGuidance/Drag.py
@@ -55,7 +55,8 @@ def reconstruct(E,D,h,v,m,model):
 
     e_dot = -D*v
     
-    hE = interp1d(E[::-1], h[::-1]*1e3, fill_value=(h[-1]*1e3,h[0]*1e3), assume_sorted=True, bounds_error=False, kind='cubic')
+    # hE = interp1d(E[::-1], h[::-1]*1e3, fill_value=(h[-1]*1e3,h[0]*1e3), assume_sorted=True, bounds_error=False, kind='cubic')
+    hE = interp1d(E[::-1], h[::-1]*1e3, fill_value="extrapolate", assume_sorted=True, bounds_error=False, kind='linear')
     dE = 1 # Might be small enough since energy is large
     h_prime = (hE(E+dE)-hE(E-dE))/2/dE
     h_prime[0] = h_prime[1]
@@ -67,7 +68,8 @@ def reconstruct(E,D,h,v,m,model):
     t = cumtrapz(-1/D/v, E, initial=0)
     
     dt = 1e-5
-    fpaT = interp1d(t, fpa, fill_value=(fpa[0],fpa[-1]), assume_sorted=True, bounds_error=False, kind='cubic')
+    # fpaT = interp1d(t, fpa, fill_value=(fpa[0],fpa[-1]), assume_sorted=True, bounds_error=False, kind='cubic')
+    fpaT = interp1d(t, fpa, fill_value="extrapolate", assume_sorted=True, bounds_error=False, kind='linear')
     gamma_dot = (fpaT(t+dt)-fpaT(t-dt))/2/dt
     gamma_dot[0]=gamma_dot[1]
     gamma_dot[-1]=gamma_dot[-2]
@@ -83,6 +85,7 @@ if __name__ == '__main__':
     from Uncertainty import getUncertainty
     from InitialState import InitialState
     from MCF import mcsplit, mcfilter
+    import chaospy as cp
     
     # Plan the nominal profile:
     reference_sim = Simulation(cycle=Cycle(0.1),output=False,**EntrySim())
@@ -92,104 +95,124 @@ if __name__ == '__main__':
     # output = reference_sim.run(x0,[bankProfile]) 
     output = reference_sim.run(x0,[bankProfile],[0,-.15,-0.15,0.0]) #Plan with less lift
     references = reference_sim.getRef()
-
-    # track_drag = 1
-    for track_drag in range(2):
+    N = 50
+    colors = ['b','r','m','g','k']
+    # labels = ['CD','CL','rho0','hs','Total']
+    labels = ['Total']
     
-        if track_drag:
-            # istart = np.argmax(output[:,1])
-            istart = np.where(output[:,13] > 8)[0][0]
-            iv = output[istart:,1]
-        else:
-            istart = np.argmax(output[:,7])
-            iv = output[istart:,7]
-            
-        energy = output[istart:,1]
-        time = output[istart:,0]
-        altitude = output[istart:,3]
-        velocity = output[istart:,7]
-        drag = output[istart:,13] #/np.cos(np.radians(output[istart:,8]))
-        # lift = output[istart:,12]
-        # sstart = output[istart,10] 
-        sstart = (reference_sim.history[0,6]-reference_sim.history[istart,6])/1000 
-        # reference_sim.plot(plotEnergy=True)  
+    delta = cp.Normal(0, 1).sample(N)
+    sample_sets = [ #np.array([delta*0.15/3, np.zeros(N), np.zeros(N), np.zeros(N)]),       # Drag dispersions
+                    #np.array([np.zeros(N), delta*0.15/3, np.zeros(N), np.zeros(N)]),       # Lift dispersions
+                    #np.array([np.zeros(N), np.zeros(N), delta*0.05, np.zeros(N)]),         # Density at h=0 dispersions
+                    #np.array([np.zeros(N), np.zeros(N), np.zeros(N), delta*0.02/3]),       # Scale height dispersions
+                    getUncertainty()['parametric'].sample(N, 'S')]                         # Full dispersion set, sampled by Sobol' sequence
+    
+    for samples, color, label in zip(sample_sets, colors, labels):  
+        print "Running deltas on {}, plotted with color {}".format(label,color)
+        for track_drag in range(2):
         
-        # samples = [np.zeros(4)]
-        # samples = np.array([np.zeros(4), [-.2, -.1, .1, .2], np.zeros(4), np.zeros(4)]) # Lift dispersions
-        # samples = np.array([[-.2, -.1, .1, .2], np.zeros(4), np.zeros(4), np.zeros(4)]) # Drag dispersions
-        
-        samples = getUncertainty()['parametric'].sample(200, 'S')
-        
-        # pdf = getUncertainty()['parametric'].pdf(samples)
-        hdata = []
-        sdata = []
-        fdata = []
-        udata = []
-        
-        for sample in samples.T:
             if track_drag:
-                h, v = solve(energy, drag, x0[7], model = EDL(sample))
-            else:    
-                h, v = output[istart:,3], output[istart:,7]
-                energy = None
-            fpa, s, u, t = reconstruct(energy, drag, h, v, x0[7]*np.ones_like(h), model = EDL(sample))
+                # istart = np.argmax(output[:,1])
+                istart = np.where(output[:,13] > 8)[0][0]
+                iv = output[istart:,1]
+                xlabel = 'Energy'
+                title = 'D(E)'
+            else:
+                istart = np.argmax(output[:,7])
+                iv = output[istart:,7]
+                xlabel = 'Velocity (m/s)'
+                title = 'h(V)'
+            energy = output[istart:,1]
+            time = output[istart:,0]
+            altitude = output[istart:,3]
+            velocity = output[istart:,7]
+            drag = output[istart:,13] #/np.cos(np.radians(output[istart:,8]))
+            # lift = output[istart:,12]
+            # sstart = output[istart,10] 
+            sstart = (reference_sim.history[0,6]-reference_sim.history[istart,6])/1000 
+            # reference_sim.plot(plotEnergy=True)  
             
-            hdata.append(h)
-            sdata.append(s)
-            fdata.append(fpa)
+
+            # pdf = getUncertainty()['parametric'].pdf(samples)
+            hdata = []
+            sdata = []
+            fdata = []
+            udata = []
+            
+            for sample in samples.T:
+                if track_drag:
+                    h, v = solve(energy, drag, x0[7], model = EDL(sample))
+                else:    
+                    h, v = output[istart:,3], output[istart:,7]
+                    energy = None
+                fpa, s, u, t = reconstruct(energy, drag, h, v, x0[7]*np.ones_like(h), model = EDL(sample))
+                
+                hdata.append(h)
+                sdata.append(s)
+                fdata.append(fpa)
+                
+                if True:
+                    plt.figure(1+track_drag)
+                    plt.plot(iv,h,'m',alpha=0.1)
+                    
+                    plt.figure(3+track_drag)
+                    plt.plot(iv,s+sstart-2.06,'m',alpha=0.1)       
+                    
+                    plt.figure(7+track_drag)
+                    plt.plot(iv,fpa,'m',alpha=0.1)
+                    
+                plt.figure(5+track_drag)
+                plt.plot(iv,u,color,alpha=0.1) 
+                plt.xlabel(xlabel)
+                plt.ylabel('u=cos(sigma)')
+
+            sdata = np.array(sdata).T + sstart - 2.06
+            s_mean = np.mean(sdata, axis=1)
+            s_var = np.diag(np.cov(sdata))
+            s_std = np.sqrt(s_var)
+            s_upper = s_mean + 3*s_std
+            s_lower = s_mean - 3*s_std
+            
+            hdata = np.array(hdata).T
+            h_mean = np.mean(hdata, axis=1)
+            h_var = np.diag(np.cov(hdata))
+            h_std = np.sqrt(h_var)
+            h_upper = h_mean + 3*h_std
+            h_lower = h_mean - 3*h_std
+            
+            fdata = np.array(fdata).T
+            f_mean = np.mean(fdata, axis=1)
+            f_var = np.diag(np.cov(fdata))
+            f_std = np.sqrt(f_var)
+            f_upper = f_mean + 3*f_std
+            f_lower = f_mean - 3*f_std
             
             plt.figure(1+track_drag)
-            plt.plot(iv,h,'m',alpha=0.1,label="pert: {}".format(sample))
+            plt.plot(iv, h_upper, '--',lw = 3, label=label, color=color)
+            plt.plot(iv, h_lower, '--',lw = 3, color=color)
+            plt.plot(iv, h_mean, lw = 3, color=color)
+            plt.xlabel(xlabel)
+            plt.ylabel('Altitude (km)')
+            plt.title(title)
             
             plt.figure(3+track_drag)
-            plt.plot(iv,s+sstart-2.06,'m',alpha=0.1,label="pert: {}".format(sample))
-            
-            plt.figure(5+track_drag)
-            plt.plot(iv,u,'m',alpha=0.1,label="pert: {}".format(sample))        
+            plt.plot(iv, s_upper, '--',lw = 3, label=label, color=color)
+            plt.plot(iv, s_lower, '--',lw = 3, color=color)
+            plt.plot(iv, s_mean, lw = 3, color=color)
+            plt.xlabel(xlabel)
+            plt.ylabel('Range (km)')
+            plt.title(title)
             
             plt.figure(7+track_drag)
-            plt.plot(iv,fpa,'m',alpha=0.1,label="pert: {}".format(sample))
-            
-
-        sdata = np.array(sdata).T + sstart - 2.06
-        s_mean = np.mean(sdata, axis=1)
-        s_var = np.diag(np.cov(sdata))
-        s_std = np.sqrt(s_var)
-        s_upper = s_mean + 3*s_std
-        s_lower = s_mean - 3*s_std
-        
-        hdata = np.array(hdata).T
-        h_mean = np.mean(hdata, axis=1)
-        h_var = np.diag(np.cov(hdata))
-        h_std = np.sqrt(h_var)
-        h_upper = h_mean + 3*h_std
-        h_lower = h_mean - 3*h_std
-        
-        fdata = np.array(fdata).T
-        f_mean = np.mean(fdata, axis=1)
-        f_var = np.diag(np.cov(fdata))
-        f_std = np.sqrt(f_var)
-        f_upper = f_mean + 3*f_std
-        f_lower = f_mean - 3*f_std
-        
-        plt.figure(1+track_drag)
-        plt.plot(iv, h_upper, 'k--',lw = 3)
-        plt.plot(iv, h_lower, 'k--',lw = 3)
-        plt.plot(iv, h_mean, 'k',lw = 3)
-        
-        plt.figure(3+track_drag)
-        plt.plot(iv, s_upper, 'k--',lw = 3)
-        plt.plot(iv, s_lower, 'k--',lw = 3)
-        plt.plot(iv, s_mean, 'k',lw = 3)
-        
-        plt.figure(7+track_drag)
-        plt.plot(iv, f_upper, 'k--',lw = 3)
-        plt.plot(iv, f_lower, 'k--',lw = 3)
-        plt.plot(iv, f_mean, 'k',lw = 3)
-    
+            plt.plot(iv, f_upper, '--',lw = 3, label=label, color=color)
+            plt.plot(iv, f_lower, '--',lw = 3, color=color)
+            plt.plot(iv, f_mean, lw = 3, color=color)
+            plt.xlabel(xlabel)
+            plt.ylabel('FPA (deg)')
+            plt.title(title)
     # mcsplit(samples, hdata, criteria)
     
-    # plt.legend(loc='best')
+    plt.legend(loc='best')
     plt.show()
     
     # import chaospy as cp
