edf1ed6d922bac751f597f6a191cb8009bcc7cd3
CDNoyes <aero.noyes@gmail.com>
2017-01-04 21:36:36 -0800

    All set to Gaussian for now, provides better results for variance analysis.

--- EntryGuidance/Uncertainty.py
+++ EntryGuidance/Uncertainty.py
@@ -14,10 +14,13 @@ def getUncertainty(parametric=True, initial=False, knowledge=False):
 
     if parametric:
         # Define Uncertainty Joint PDF
-        CD          = cp.Uniform(-0.10, 0.10)   # CD
-        CL          = cp.Uniform(-0.10, 0.10)   # CL
+        # CD          = cp.Uniform(-0.15, 0.15)   # CD
+        CD          = cp.Normal(0, 0.15/3)   # CD
+        # CL          = cp.Uniform(-0.15, 0.15)   # CL
+        CL          = cp.Normal(0, 0.15/3)   # CL
         rho0        = cp.Normal(0, 0.0333*1.5)      # rho0
-        scaleHeight = cp.Uniform(-0.02,0.01)    # scaleheight        
+        # scaleHeight = cp.Uniform(-0.02,0.01)    # scaleheight        
+        scaleHeight = cp.Normal(0,0.02/3)    # scaleheight        
         perturbations['parametric'] = cp.J(CD,CL,rho0,scaleHeight)
         
     if knowledge:
