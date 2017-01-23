831f6989d9064432ad24ba5ccfbec0cd708427ac
CDNoyes <aero.noyes@gmail.com>
2017-01-22 18:32:12 -0800

    Some changes to distributions, and small clean up. No real changes otherwise.

--- Misc/VDP.py
+++ Misc/VDP.py
@@ -3,13 +3,10 @@ import chaospy as cp
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-# boxspace(xmin,xmax,ymin,ymax):
-
 
 
 class VDP(object):
     ''' A van der pol oscillator class '''
-    
     
     def __init__(self):
         print "Initialized"
@@ -36,7 +33,7 @@ class VDP(object):
         self.samples = samples
         self.pdf = pdf
         
-    def plot(self, PF):
+    def plot(self):
         ''' Visualizations of the monte carlo results. '''
         if 0:
             for traj in self.outputs:
@@ -48,26 +45,41 @@ class VDP(object):
                 # plt.scatter(self.outputs[:,i,0],self.outputs[:,i,1],20,self.pdf)
                 plt.scatter(self.outputs[:,i,0],self.outputs[:,i,1],20,self.outputs[:,i,2])
             
-        if PF:    
-            plt.figure(2)
-            plt.scatter(self.outputs[:,-1,0],self.outputs[:,-1,1],20,self.outputs[:,-1,2],vmin=0) #PF operator
-            plt.title('PF Operator')
-            plt.colorbar()
-        else:    
-            plt.figure(3)
-            plt.hist2d(self.outputs[:,-1,0], self.outputs[:,-1,1], normed=True, bins=(100,100))
-            plt.title('QMC')    
-            plt.colorbar()
+            
+        plt.figure(3)
+        counts,xe,ye,im = plt.hist2d(self.outputs[:,-1,0], self.outputs[:,-1,1], normed=True, bins=(250,250))
+        plt.title('QMC')    
+        plt.colorbar()    
+            
+        # print type(counts)    
+        # print counts.shape    
+            
+        vmax = np.max((counts.max(),self.outputs[:,-1,2].max()))    
+            
+        plt.figure(2)
+        plt.scatter(self.outputs[:,-1,0],self.outputs[:,-1,1],20,self.outputs[:,-1,2],vmin=0) #PF operator
+        plt.scatter(self.outputs[:,-1,0],self.outputs[:,-1,1],20,self.outputs[:,-1,2],vmin=0,vmax=vmax) #force same color range as the histogram
+        plt.title('PF Operator')
+        plt.colorbar()
+
             
         
     def test(self):
+        ''' 
+            Runs the VDP tests:
+                Monte Carlo 
+                Perron-Frobenius Operator
+            
+        '''
         if 0:
             N1 = cp.Normal(3,0.1)
             N2 = cp.Normal(0,0.3)
             MU = cp.Normal(0.1,.1)
-        elif 0:
+        elif 1:
             N1 = - 0.5+cp.Beta(2,5)
-            N2 = cp.Beta(1,1)-0.5
+            # N2 = cp.Beta(1.5,2)-0.5
+            N2 = cp.Normal(0,0.3)
+
             MU = cp.Uniform(0.,.0005)
 
         else:
@@ -79,24 +91,24 @@ class VDP(object):
         # delta = cp.J(N1,N2,MU)
         delta = cp.J(N1,N2)
         
-        tf = 1
+        tf = 10
         Mu = 0.5
         
-        samples = delta.sample(200000,'S').T
+        samples = delta.sample(200000,'L').T
         pdf = delta.pdf(samples.T)
         samples=np.append(samples,Mu*np.ones((samples.shape[0],1)),1)
+                  
         self.monte_carlo(samples,tf,pdf)
-        self.plot(False)
+        self.plot()
                 
-        # xy = np.mgrid[2.7:3.3:15j, -0.9:0.9:75j].reshape(2,-1).T
+        # xy = np.mgrid[2.7:3.3:50j, -0.9:0.9:50j].reshape(2,-1).T
         # mu = Mu*np.ones((xy.shape[0],1))
 
         # grid = np.append(xy, mu, axis=1)
-        # pdf_grid = delta.pdf(grid.T)
         # pdf_grid = delta.pdf(xy.T)
 
         # self.monte_carlo(grid,tf,pdf_grid)
-        self.plot(True)
+        # self.plot()
 
         plt.show()    
 
