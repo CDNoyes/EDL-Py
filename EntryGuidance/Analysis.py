5b7adfe7a0fbbdccbae11d2f5a4eb6276ad8818d
Connor Noyes <cdnoyes@jpl.nasa.gov>
2017-02-04 09:55:41 -0800

    Basic analysis script for use with MC data. Defines small functions for partitioning the data and applies MCF.

---
+++ EntryGuidance/Analysis.py
@@ -0,0 +1,45 @@
+""" Scripts and methods for analysis of monte carlo data """
+
+import numpy as np
+import MCF
+    
+from scipy.io import savemat, loadmat
+import matplotlib.pyplot as plt
+
+
+def filter():
+    mcdata = loadmat('./data/MC_Apollo_1000.mat')
+    
+    inputs = mcdata['samples']
+    outputs = mcdata['states'].flatten()
+    
+    print "Cases on the lower altitude boundary:"
+    data = MCF.mcsplit(inputs,outputs,low_alt)
+    MCF.mcfilter(*data, input_names=['CD','CL','p0','hs'], plot=False) 
+    
+    print "Cases more than 10 m/s different than nominal ignition velocity:"
+    data = MCF.mcsplit(inputs,outputs,high_vel)
+    MCF.mcfilter(*data, input_names=['CD','CL','p0','hs'], plot=False) 
+    
+    print "Cases more than 5 km from the target downrange:"
+    data = MCF.mcsplit(inputs,outputs,large_miss)
+    MCF.mcfilter(*data, input_names=['CD','CL','p0','hs'], plot=True) 
+    
+    
+def low_alt(output):
+    xf = output[-1]
+    alt = xf[3] # in km
+    return alt > 0.6
+    
+def high_vel(output):
+    xf = output[-1]
+    vel = xf[7] # in km
+    return vel < 510 and vel > 490
+    
+def large_miss(output):    
+    xf = output[-1]
+    range = np.abs(output[-1,10]-905) # Range to go
+    return range < 5
+
+if __name__ == "__main__":
+    filter()
