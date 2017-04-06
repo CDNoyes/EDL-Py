3faa400bbf8b3cdf53df7f34fbbdb40b38f206ec
Connor Noyes <cdnoyes@jpl.nasa.gov>
2017-04-05 22:06:04 -0700

    Added a small but important change to Rk4 implementation.

--- Utils/RK4.py
+++ Utils/RK4.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def RK4(fun, x0, iv, args):
     """ Pure python implementation of a common 4th-order Runge Kutta integrator """
-    x = [np.array(x0)]
+    x = [np.asarray(x0)]
     div = np.diff(iv)
     for t,dt in zip(iv,div):
         x.append(rk4_step(fun, t, x[-1], dt, args))
@@ -10,7 +10,7 @@ def RK4(fun, x0, iv, args):
 
 def rk4_step(f, iv, x, h, args):
     """ Takes a single 4th-order step """
-    k1 = f(x,          iv,       *args)
+    k1 = f(x[:],       iv,       *args)
     k2 = f(x+0.5*h*k1, iv+0.5*h, *args)
     k3 = f(x+0.5*h*k2, iv+0.5*h, *args)
     k4 = f(x+    h*k3, iv+h,     *args)  
