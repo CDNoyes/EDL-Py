c3842278c90f31c877d36c28b0c5e2f43d345465
Connor Noyes <cdnoyes@jpl.nasa.gov>
2017-04-27 17:48:30 -0700

    Some fixes to ProjectedNewton, now works for systems of any dimension, including scalar.

--- Utils/ProjectedNewton.py
+++ Utils/ProjectedNewton.py
@@ -3,7 +3,7 @@
 
 import numpy as np 
 
-def ProjectedNewton(x0, hessian, gradient, bounds, tol=1e-6):
+def ProjectedNewton(x0, hessian, gradient, bounds, tol=1e-6, debug=False):
     """
     Solves the quadratic problem:
     min F(x) = 0.5*x'*hessian*x + gradient'*x
@@ -22,32 +22,54 @@ def ProjectedNewton(x0, hessian, gradient, bounds, tol=1e-6):
         fopt        -   the value of the quadratic objective function evaluated at x 
     
     """
+    
+    if debug:
+        print "Inputs:  "
+        print "Hessian = {}".format(hessian)
+        print "Gradient = {}".format(gradient)
+        print "Initial guess = {}".format(x0)
+    
     iter = 0
     iterMax = 100
     n = len(x0)
     
     x = np.clip(x0, bounds[0],bounds[1]) # Make the initial point feasible 
+    if n > 1:
+        x = x[0]
     xl = np.asarray(bounds[0])
     xu = np.asarray(bounds[1])
     hessian = np.asarray(hessian)
     gradient = np.asarray(gradient)
     
+    if debug:
+        print "After some transforms:  "
+        print "Hessian = {}".format(hessian)
+        print "Gradient = {}".format(gradient)
+        print "Initial feasible guess = {}\n".format(x)
+    
     while iter < iterMax:
-        g = gradient + np.dot(hessian, x)
-        
+        g = gradient + np.dot(hessian, x.T).T
+ 
         # Determine the active set (and the complementary inactive set)
         idu = (x-xu) == 0
         idl = (xl-x) == 0
         gu  = g>0
         gl  = g<0 
+        if debug:
+            print "g.shape = {}".format(g.shape)
 
         c = ((gl.astype(int)+idu.astype(int)) > 1) + ((gu.astype(int)+idl.astype(int)) > 1)
         f = ~c.astype(bool) 
-        f = np.where(f)[0] # bool array to raw indices 
+        f = np.where(f[0])[0]
 
         hff =  hessian[f,:][:,f]
+        if debug:
+            print "Free dirs: {}".format(f)
+            print "Free hess: {}".format(hff)
+            print "Free g: {}".format(gradient[f])
+            print "Free x: {}".format(x[f])
         if len(f):
-            gf = gradient[f] + np.dot(hff,x[f])
+            gf = gradient[f] + np.dot(hff,x[f].T).T
         else:
             gf = np.zeros_like(gradient)
 
@@ -65,13 +87,18 @@ def ProjectedNewton(x0, hessian, gradient, bounds, tol=1e-6):
 
         alpha = armijo(fQuad(hessian,gradient), x, dx, g, xl, xu)
         x = np.clip(x+alpha*dx, xl, xu)
+        if n > 1:
+            x = x[0]
         iter += 1
         
-    fopt = fQuad(hessian,gradient)(x)
+    # fopt = fQuad(hessian,gradient)(x)
     # print "Total iterations: {}".format(iter)
     # print x 
     # print fopt 
-    return x, hff, fopt
+    clamped = np.where(c[0])[0]
+    H = hessian
+    H[clamped,:][:,clamped] = 0
+    return x, H # To do: return the full Hessian with clamped elements set to zero already, rather than do it outside 
     
 def armijo(f,x,dx,g,xl,xu):
     gamma = 0.1
@@ -80,13 +107,13 @@ def armijo(f,x,dx,g,xl,xu):
     r = 0.5*gamma 
     while r<gamma:
         alpha *= c 
-        xa = np.clip(x+alpha*dx,xl,xu)
-        r = (f(x)-f(xa))/np.dot(g.T, (x-xa))
+        xa = np.clip(x+alpha*dx,xl,xu)[0]
+        r = (f(x)-f(xa))/np.dot(g, (x-xa).T)
     return alpha 
         
 def fQuad(h,g):
     def fQuadFun(x):
-        return 0.5*np.dot(x.T, np.dot(h,x)) + np.dot(g.T,x)
+        return 0.5*np.dot(x, np.dot(h,x.T)) + np.dot(g,x)
     return fQuadFun
 
         
