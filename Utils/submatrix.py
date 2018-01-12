0dd09a05bd8203c241f6eed1ddb7caa7c5539c7f
Connor Noyes <aero.noyes@gmail.com>
2018-01-12 12:01:13 -0800

    Added useful default arguments in submatrix, because often we want a square submatrix.

--- Utils/submatrix.py
+++ Utils/submatrix.py
@@ -1,19 +1,21 @@
 import numpy as np
 
-def submatrix(M,rows,cols):
+def submatrix(M,rows,cols=None):
     """ From a matrix M returns the elements M[rows,cols] """
+    if cols is None:
+        cols=rows 
     M = np.asarray(M)
     S = M[rows,:][:,cols]
 
     return S
 
 def test_submatrix():
-    
+
     matrix = np.random.random((5,5))
     print matrix
     sub = submatrix(matrix,[0,2],[0,2])
-    print sub 
-    
-    
+    print sub
+
+
 if __name__ == "__main__":
-    test_submatrix() 
+    test_submatrix()
