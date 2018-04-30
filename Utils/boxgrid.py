e014981a4d20fd970c3b6e1f15fb59e44bc3dba2
Connor Noyes <cdnoyes@jpl.nasa.gov>
2018-04-29 19:27:38 -0700

    Another small update to boxgrid.

--- Utils/boxgrid.py
+++ Utils/boxgrid.py
@@ -1,18 +1,20 @@
 from itertools import product 
 import numpy as np
 
+
 def boxgrid(bounds, N, interior=False):
-    """ n-dimensional box grid, either just the exterior, or with interior points as well
-    bounds is an n-length list/tuple with each element being the (min,max) along that dimension
-    N is the number of samples per dimension, either a scalar or N-length list of integers. 
-    Set N = 2 and interior=False to get just the vertices of the hyper-rectangle.
+    """ n-dimensional box grid, either just the exterior, or with interior points as well.
+    Inputs:
+        bounds is an n-length list/tuple with each element being the (min,max) along that dimension
+        N is the number of samples per dimension, either a scalar or n-length list of integers
+        
+    Setting N=2 and interior=False will return just the vertices of the hyper-rectangle.
     
     """
-    try:
-        N[0]
-    except:
+    if isinstance(N, int):
         N = [N for _ in bounds]
         
+       
     n = len(bounds)    
     vectors = [np.linspace(b[0],b[1],ni) for b,ni in zip(bounds,N)]
 
@@ -22,25 +24,35 @@ def boxgrid(bounds, N, interior=False):
         bmax = np.array(bounds)[:,1]
         ikeep = []
         for i,pt in enumerate(grid_points):
-            # if np.any( pt-bmin == 0) or np.any( pt-bmax == 0): # This condition only works in 2D. In ND, (N-1) points must be extremal to constitute an edge 
-            if np.sum(pt-bmin == 0) + np.sum( pt-bmax == 0) >= (len(bounds)-1): 
+            # In n-dimensions, >=(n-1) points must be extremal to constitute an edge 
+            if np.count_nonzero(np.logical_or(pt-bmin == 0, pt-bmax == 0)) >= (len(bounds)-1): # Count is faster than sum for some reason 
                 ikeep.append(i)
         grid_points = grid_points[ikeep]    
-    # print('Debug, number of kept points = {}'.format(len(ikeep)))
     return grid_points
+    
     
 def test_boxgrid():
     
     # 2-d example
     import matplotlib.pyplot as plt
+    import time 
+    t0 = time.time()
     pts = boxgrid(((-3,3),(-1,1)), (7,4),interior=True)
-    plt.figure()
-    plt.plot(pts[:,0],pts[:,1],'o')
+    # plt.figure()
+    # plt.plot(pts[:,0],pts[:,1],'o')
     
     pts = boxgrid(((-3,3),(-1,1)), 5, interior=False)
-    plt.figure()
-    plt.plot(pts[:,0],pts[:,1],'o')
-  
+    # plt.figure()
+    # plt.plot(pts[:,0],pts[:,1],'o')
+    
+    # Demonstrate that the correct number of vertices is returned for higher dimensions 
+    n = 10
+    bounds = [(0,1)]*n
+    N = 2
+    pts = boxgrid(bounds, N, interior=False)
+    assert(len(pts)==N**10)
+
+    print("Time elapsed: {} s".format(time.time()-t0))
     
     plt.show()    
     
