5e4fcb97b7c08b7b1ecc4d41b2a30225218b2749
cdnoyes <aero.noyes@gmail.com>
2018-06-08 20:52:25 -0700

    Updated some DA functions to work properly with vectorized duals

--- Utils/DA.py
+++ Utils/DA.py
@@ -5,6 +5,11 @@ from pyaudi import gdual_double as gd
 from pyaudi import gdual_vdouble as gdv
 from pyaudi import abs
 
+from .memoize import memoize
+
+dual = (gd, gdv)
+
+
 def odeint(fun, x0, iv, args=(), min_order=4, max_order=40, tol=1e-3):
 
     x_collect = [x0]
@@ -14,30 +19,32 @@ def odeint(fun, x0, iv, args=(), min_order=4, max_order=40, tol=1e-3):
     zero_pt = np.zeros_like(eval_pt).tolist()
     div = np.diff(iv)
 
-    for t, dt in zip(iv,div):
-        x = np.array([gd(0, var, 1) for var in vars[:-1]]) # Initialize
+    for t, dt in zip(iv, div):
+        x = np.array([gd(0, var, 1) for var in vars[:-1]])  # Initialize
         for order in range(2, max_order):
             x_next = _step(fun, t, x0, x, dt, order, *args)
-            change = np.linalg.norm(evaluate(x-x_next,vars,[eval_pt+[dt]])[0][0]) # norm of just the new terms
+            change = np.linalg.norm(evaluate(x-x_next, vars, [eval_pt+[dt]])[0][0])  # norm of just the new terms
             # Update for next iteration
             x = x_next
             # Convergence criteria
             if change <= tol and order >= min_order:
                 # print("Terminating at order = {}".format(order))
                 break
-        x0 = evaluate(x,vars,[zero_pt+[dt]])[0] # evaluate the endpoint model
+        x0 = evaluate(x, vars, [zero_pt+[dt]])[0]  # evaluate the endpoint model
         x_collect.append(x0.squeeze())
     return np.array(x_collect).squeeze()
 
+
 def _step(fun, t0, x0, x, dt, order, *args):
 
-    x0 = np.array([gd(x0i, 'x{}'.format(i), order) for i,x0i in enumerate(x0)])
+    x0 = np.array([gd(x0i, 'x{}'.format(i), order) for i, x0i in enumerate(x0)])
     t = gd(t0, 't', order)
-    x+= 0*t # This is done purely to promote the order of x
+    x += 0*t  # This is done purely to promote the order of x
 
     f = fun(x, t, *args)            # Expand around the current time and state
-    F = np.array([da.integrate('t') if isinstance(da,gd) else da*t for da in f], dtype=gd) # Anti-derivation
+    F = np.array([da.integrate('t') if isinstance(da, gd) else da*t for da in f], dtype=gd)  # Anti-derivation
     return x0 + F
+
 
 def evaluate(da_array, da_vars, pts):
     """
@@ -46,28 +53,30 @@ def evaluate(da_array, da_vars, pts):
     """
 
     delta = ['d'+da_var for da_var in da_vars]
-    eval_pt = {da_var:0 for da_var in delta}    # Preallocate the evaluation dictionary
+    eval_pt = {da_var: 0 for da_var in delta}    # Preallocate the evaluation dictionary
     new_pts = []
     for pt in pts:
         try:
             pt[0]
-        except IndexError: # pt is a scalar 
+        except IndexError:  # pt is a scalar
             pt = [pt]
         # eval_pt = {da_var:element for da_var,element in zip(delta,pt)} # This is probably slow since we're constructing a new dict every time
-        eval_pt.update(zip(delta,pt))
+        eval_pt.update(zip(delta, pt))
         new_pt = [da.evaluate(eval_pt) if isinstance(da, (gd,gdv)) else da for da in da_array]
         new_pts.append(new_pt)
 
     return np.array(new_pts)
 
+
 def differentiate(da, da_vars):
     """Differentiates a generalized dual variable wrt da_vars"""
     g = np.zeros(len(da_vars), dtype=gd)
-    if isinstance(da,gd):
+    if isinstance(da, gd):
         for var in da.symbol_set:
             ind = da_vars.index(var)
             g[ind] = da.partial(var)
     return g
+
 
 def gradient(da, da_vars):
     """Returns the numerical value of the gradient of a generalized dual variable
@@ -76,15 +85,17 @@ def gradient(da, da_vars):
             da_vars is 1-d list/array with string names in the order the gradient should be given
     """
     g = np.zeros(len(da_vars))
-    if isinstance(da,gd): # It may be the case that the DA was differentiated and became a constant
+    if isinstance(da, gd): # It may be the case that the DA was differentiated and became a constant
         for var in da.symbol_set:
             ind = da_vars.index(var)
             g[ind] = da.partial(var).constant_cf
     return g
 
+
 def jacobian(da_array, da_vars):
     """ Forms the Jacobian matrix, i.e. the gradient of a vector valued function """
     return np.array([gradient(da, da_vars) for da in da_array])
+
 
 def hessian(da, da_vars):
     """ Retrieves the 2nd order coefficients forming the 2D Hessian matrix of a scalar function """
@@ -98,26 +109,27 @@ def hessian(da, da_vars):
             # ind1 = da_vars.index(var)
             # h[ind1,ind2] = gda.partial(var).constant_cf
 
-    h = [gradient(gda,da_vars) for gda in g]    # This is simple but we can cut the work nearly in half by exploiting symmetry
+    # This is simple but we can cut the work nearly in half by exploiting symmetry
+    h = [gradient(gda, da_vars) for gda in g]    
 
     return np.array(h)
 
 
 def vhessian(da_array, da_vars):
     """ Computes the tensor comprising the hessian of a vector valued function """
-    return np.array([hessian(da,da_vars) for da in da_array])
+    return np.array([hessian(da, da_vars) for da in da_array])
 
 
 def const(da_array, array=False):
     """ Collects the constant part of each generalized dual variable and returns a list or numpy array. """
     try:
-        da_array[0] # Check to see if its a scalar
+        da_array[0]  # Check to see if its a scalar
         if array:
             return np.array([const(da, array=array) for da in da_array])
         else:
             return [const(da, array=array) for da in da_array]
-    except:
-        if isinstance(da_array, gd):
+    except (IndexError, TypeError):
+        if isinstance(da_array, dual):
             return da_array.constant_cf
         else:
             return da_array
@@ -125,36 +137,45 @@ def const(da_array, array=False):
 
 def const_dict(da_dict):
     con_dict = {}
-    for key,val in da_dict.items():
+    for key, val in da_dict.items():
         try:
             con_dict[key] = val.constant_cf
-        except:
+        except AttributeError:
             con_dict[key] = val
     return con_dict
 
-def make(values, names, orders, array=False):
-    """ Turn an array of constant values into an array of generalized duals with specified names and orders. Orders may be an integer. """
+
+def make(values, names, orders, array=False, vectorized=False):
+    """ Turn an array of constant values into an array of generalized duals
+        with specified names and orders. Orders may be an integer.
+    """
+    if vectorized:
+        cls = gdv
+    else:
+        cls = gd
     if isinstance(orders, int):
         orders = [orders for _ in names]
     if array:
-        return np.array([gd(v,n,o) for v,n,o in zip(values, names, orders)],dtype=gd)
+        return np.array([cls(v, n, o) for v, n, o in zip(values, names, orders)], dtype=cls)
 
     else:
-        return [gd(v,n,o) for v,n,o in zip(values, names, orders)]
+        return [cls(v, n, o) for v, n, o in zip(values, names, orders)]
 
 
 def radians(x):
     return x*np.pi/180.0
 
+
 def degrees(x):
     return x*180.0/np.pi
 
+
 def sign(x):
-    if isinstance(x,gd):
+    if isinstance(x, dual):
         xc = x.constant_cf
     else:
         xc = x
-    if abs(xc)<1e-6:
+    if abs(xc) < 1e-6:
         return x-xc             # So its still a DA variable but with absolutely 0 part
     else:
         return xc/abs(xc)       # This will never be a DA variable so we can use the numerical value
@@ -178,7 +199,6 @@ def sign(x):
 #       c = (c[1:-1-k] - c[:-2-k])* k / dt => c = (c[1:-1-k] - c[:-2-k])*(k/dt)    # For some reason, the dual variables couldn't be divided by a float, but multiplying worked fine
 
 
-from .memoize import memoize
 
 @memoize
 def __getSplIdx(x,t):
@@ -186,27 +206,27 @@ def __getSplIdx(x,t):
     for xi in x:
         for i,ti in enumerate(t):
             if ti > xi:
-                idx.append(i-1) # This gives the index of the lower bound such that t[idx] < xi < t[idx+1]
+                idx.append(i-1)  # This gives the index of the lower bound such that t[idx] < xi < t[idx+1]
                 break
     return idx
 
 
 def splev(x, spline):
     """ Implements De Boor's algorithm to evaluate a B-spline at points in x """
-    if isinstance(spline,tuple):
+    if isinstance(spline, tuple):
         t,c,n = spline
     else:
         t,c,n = spline.tck
 
-    L = __getSplIdx(x,t)
+    L = __getSplIdx(x, t)
     Ln = [Li-n for Li in L]
 
     B = []
-    for j,xj  in enumerate(x):
+    for j, xj  in enumerate(x):
         d = np.array(c[Ln[j]:L[j]+1])
         dnext = np.zeros_like(d)
 
-        for k in range(1,n+1):
+        for k in range(1, n+1):
             for i in range(Ln[j]+k,L[j]+1):
                 idx = i-Ln[j]
                 alpha_kn = (xj - t[i])/(t[i+n+1-k] - t[i])
