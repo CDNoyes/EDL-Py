7fe298ba786f96517beeb854b2079f69730e39a6
cdnoyes <aero.noyes@gmail.com>
2018-06-10 11:04:10 -0700

    Small formatting change and added Chebyshev option for unevenly spaced points.

--- Utils/boxgrid.py
+++ Utils/boxgrid.py
@@ -2,11 +2,13 @@ from itertools import product
 import numpy as np
 
 
-def boxgrid(bounds, N, interior=False):
+def boxgrid(bounds, N, interior=False, chebyshev=False):
     """ n-dimensional box grid, either just the exterior, or with interior points as well.
     Inputs:
         bounds is an n-length list/tuple with each element being the (min,max) along that dimension
         N is the number of samples per dimension, either a scalar or n-length list of integers
+        interior indicates whether or not only surface points are kept
+        chebyshev uses Chebyshev nodes instead of equal spacing 
 
     Setting N=2 and interior=False will return just the vertices of the hyper-rectangle.
 
@@ -14,18 +16,20 @@ def boxgrid(bounds, N, interior=False):
     if isinstance(N, int):
         N = [N for _ in bounds]
 
-
     n = len(bounds)
-    vectors = [np.linspace(b[0],b[1],ni) for b,ni in zip(bounds,N)]
+    if chebyshev:
+        vectors = [0.5*(1-np.cos(np.pi*np.arange(0, ni)/(ni-1)))*(b[1]-b[0])+b[0] for b, ni in zip(bounds, N)]
+    else:
+        vectors = [np.linspace(b[0], b[1], ni) for b, ni in zip(bounds, N)]
 
     grid_points = np.array(list(product(*vectors)))
     if not interior:                # Remove the interior points
         bmin = np.array(bounds)[:,0]
         bmax = np.array(bounds)[:,1]
         ikeep = []
-        for i,pt in enumerate(grid_points):
+        for i, pt in enumerate(grid_points):
             # In n-dimensions, >=(n-1) points must be extremal to constitute an edge
-            if np.count_nonzero(np.logical_or(pt-bmin == 0, pt-bmax == 0)) >= (len(bounds)-1): # Count is faster than sum for some reason
+            if np.count_nonzero(np.logical_or(pt-bmin == 0, pt-bmax == 0)) >= (len(bounds)-1):  # Count is faster than sum for some reason
                 ikeep.append(i)
         grid_points = grid_points[ikeep]
     return grid_points
@@ -37,23 +41,23 @@ def test_boxgrid():
     import matplotlib.pyplot as plt
     import time
 
-    pts = boxgrid(((-3,3),(-1,1)), (7,4),interior=True)
+    pts = boxgrid(((-3, 3),(-1, 1)), (10, 6), interior=True, chebyshev=True)
     plt.figure()
-    plt.plot(pts[:,0],pts[:,1],'o')
+    plt.plot(pts[:,0], pts[:,1], 'o')
 
-    pts = boxgrid(((-3,3),(-1,1)), 5, interior=False)
+    pts = boxgrid(((-3, 3), (-1, 1)), 5, interior=False, chebyshev=True)
     plt.figure()
-    plt.plot(pts[:,0],pts[:,1],'o')
+    plt.plot(pts[:,0], pts[:,1], 'o')
 
     # Demonstrate that the correct number of vertices is returned for higher dimensions
-    n = 10
-    bounds = [(0,1)]*n
-    N = 2
-    t0 = time.time()
-    pts = boxgrid(bounds, N, interior=False)
-    assert(len(pts)==N**10)
+    # n = 10
+    # bounds = [(0,1)]*n
+    # N = 2
+    # t0 = time.time()
+    # pts = boxgrid(bounds, N, interior=False)
+    # assert(len(pts)==N**10)
 
-    print("Time elapsed: {} s".format(time.time()-t0))
+    # print("Time elapsed: {} s".format(time.time()-t0))
 
     plt.show()
 
