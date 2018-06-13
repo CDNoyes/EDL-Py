a7e766ab30a9ec0709e9595a93760a5104919cd8
cdnoyes <aero.noyes@gmail.com>
2018-06-12 17:44:38 -0700

    Adds a useful and tunable, smooth saturation function of symmetric bounds

--- Utils/smooth_sat.py
+++ Utils/smooth_sat.py
@@ -1,6 +1,7 @@
 import numpy as np
 from pyaudi import gdual_double as gd
-from pyaudi import exp
+from pyaudi import gdual_vdouble as gdv
+import pyaudi as pa 
 
 def smooth_sat(y, b=5.522):
     """
@@ -10,18 +11,25 @@ def smooth_sat(y, b=5.522):
         For the interval [0,1] the optimal values = [ 1., 5.3013]
         For the interval [0,0.96] the optimal values = [ 0.96., 5.522]
     """
-    a = 0.96 # Maximum value of the saturation function
+    a = 0.96  # Maximum value of the saturation function
     c = a/2.
     try:
-        if isinstance(y[0],gd):
-            return np.array([a/(1+exp(-b*(yi-c))) for yi in y])
+        if isinstance(y[0], (gd, gdv)):
+            return np.array([a/(1+pa.exp(-b*(yi-c))) for yi in y])
         else:
             return a/(1+np.exp(-b*(y-c)))
-    except:
-        return a/(1+exp(-b*(y-c)))
+    except (IndexError, TypeError):
+        return a/(1+pa.exp(-b*(y-c)))
 
+def symmetric_sat(x, bound, tuning_parameter=1e-3):
+    """ From Avvakumov et al
+        The smaller the tuning parameter is, the more tightly this will
+         approximate the saturation function from [-bound, bound].
+    """
+    return 0.5*bound*(pa.sqrt(tuning_parameter + (x/bound + 1)**2) -
+                      pa.sqrt(tuning_parameter + (x/bound - 1)**2))
 
-def tanh_sat(y,k,y0):
+def tanh_sat(y, k, y0):
     """
     The logistic function and hyberbolic tangent are related and can produce identical results.
     Optimal = [2.65,0.5]
@@ -29,9 +37,17 @@ def tanh_sat(y,k,y0):
     return 0.5 + 0.5*np.tanh(k*(y-y0))
 
 
-def atan_sat(y,k,y0,c,a):
+def atan_sat(y, k, y0, c, a):
     return c + a*(np.arctan(k*(y-y0)) - np.arctan(-k*(y-y0)))
 
+
+def erf_sat(y):
+    """ This is appropriate for saturating to [-1,1] """
+    if isinstance(y, (gd, gdv)):
+        return pa.erf(pa.sqrt(np.pi)*0.5*y)
+    else:
+        from scipy.special import erf
+        return erf(np.sqrt(np.pi)*0.5*y)
 
 
 def opt():
@@ -39,10 +55,11 @@ def opt():
     import matplotlib.pyplot as plt
 
     y_raw = np.linspace(-2.5,3.5,500)
-    y = np.clip(y_raw,0,0.96)
+    y = np.clip(y_raw,-1,1)
+    # y = np.clip(y_raw,0,0.96)
 
     aopt = curve_fit(smooth_sat, y_raw, y,bounds=(0,10))
-    print "(Logistic) Optimal values = {}".format(aopt[0])
+    print("(Logistic) Optimal values = {}".format(aopt[0]))
 
     # topt = curve_fit(tanh_sat, y_raw, y)
     # print "(Tanh) Optimal values = {}".format(topt[0])
