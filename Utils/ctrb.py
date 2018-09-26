3cd3c337b38b4e8fb253351066a963205dcd67b0
Connor Noyes <cdnoyes@jpl.nasa.gov>
2018-09-25 23:01:48 -0700

    Adds basic utils for constructing controllability matrix and checking its rank

---
+++ Utils/ctrb.py
@@ -0,0 +1,28 @@
+import numpy as np 
+
+def ctrb_mat(A,B):
+    """ Computes the controllability matrix """
+    C=[B]
+    n = A.shape[0]
+    
+    for i in range(1,n):
+        C.append(A.dot(C[-1]))
+        
+
+    return np.array(C)   
+    
+def ctrb(A,B):
+    """ Determines if (A,B) is controllable via rank condition """
+    n = A.shape[0]
+    C = ctrb_mat(A,B)
+    r = np.linalg.matrix_rank(C)
+    return r==n
+        
+if __name__ =="__main__":
+    A = np.eye(3)   # nxn
+    A = np.array([[0,1,1],[-1,-1,0],[0,0,0]])
+    B = [0,0,1]     # nxm
+    
+    C = ctrb_mat(A,B)   
+    print C.shape   # = nxnm
+    print ctrb(A,B)
