1afb6345b787cdb2341c0619b7ff7677142a2b0e
cdnoyes <aero.noyes@gmail.com>
2018-10-09 21:19:12 -0700

    Updates LQR with some fixes and improvements

--- Utils/LQR.py
+++ Utils/LQR.py
@@ -2,12 +2,12 @@ import numpy as np
 from RK4 import RK4 
 
 
-def LQR(x0, tf, A, B, Q, R, Qf):
+def LQR(tf, A, B, Q, R, Qf):
     """Computes the optimal control to a continuous time
        finite-horizon linear quadratic regulator
     """
 
-    n = np.shape(np.asarray(x0).squeeze())
+    n = np.shape(A)[0]
     m = np.shape(R)[0]
 
     if Qf is None:
@@ -16,24 +16,45 @@ def LQR(x0, tf, A, B, Q, R, Qf):
         P0 = Qf
     
     Ri = np.linalg.inv(R)
-    t = np.linspace(0, tf, 100)
+    t = np.linspace(0, tf, 1000)
     P = RK4(_dyn, P0, t, args=(A, B, Q, Ri))[::-1]
-    for Pi in P:
-        K = Ri @ B.T @ P
+    print(P[0])
+    K = Ri @ B.T @ P
 
-    return K 
+    return np.transpose(K, (0, 2, 1))
+
 
 def _dyn(P, t, A, B, Q, Ri):
 
-    Pdot = A.T @ P + P @ A + P @ B @ Ri @ B.T @ P + Q
+    Pdot = A.T @ P + P @ A - P @ B @ Ri @ B.T @ P + Q
 
     return Pdot
 
+
 if __name__ == "__main__":
-    A = np.diag([1,0])
-    B = np.array([0,1])
-    Q = np.diag([1,1])
+    import matplotlib.pyplot as plt 
+    import chaospy as cp 
+    from scipy.interpolate import interp1d
+
+    A = np.diag([1, 1])
+    B = np.array([[0, 1]]).T
+    Q = np.diag([1, 1])
     R = np.array([[1]])
-    Qf = np.diag([1,10])
-    x0 = np.random.random((2,))
-    K = LQR(x0, 5, A, B, Q, R, Qf)
+    Qf = np.diag([10, 10])
+    K = LQR(5, A, B, Q, R, Qf)
+    t = np.linspace(0, 5, 1000)
+    # plt.plot(t, K.squeeze())
+
+    N = cp.Normal(0, 1)
+    x0 = np.array( (N.sample(20), N.sample(20)))
+
+    def LTI(x,t, K):
+        k = K(t).T
+        u = -k@x
+        return A@x +B@u
+
+    Ki = interp1d(t, K, axis=0)
+    X = RK4(LTI, x0, t, args=(Ki, ))
+
+    plt.plot(t, X[:, 0])
+    plt.show()
