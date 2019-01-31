cbedc6a2b27fef24eaac9d80d0843cb03ddd33f8
cdnoyes <aero.noyes@gmail.com>
2019-01-31 08:07:26 -0800

    Updates to FOFB

--- EntryGuidance/FOFB.py
+++ EntryGuidance/FOFB.py
@@ -3,6 +3,21 @@
 import numpy as np 
 
 
+class Controller3D:
+
+    def __init__(self):
+        self.controllers = [Controller1D() for i in range(3)]
+
+    def __call__(self, state):
+        x,y,z,u,v,w = state 
+
+        ux = self.controllers[0]([x,u])
+        uy = self.controllers[0]([y,v])
+        uz = self.controllers[0]([z,w])
+
+        return [ux, uy, uz]
+
+
 class Controller1D:
     """ Provides a chatter-free, overshoot-free fuel optimal feedback control for planar, saturated double integrators """
 
@@ -81,6 +96,22 @@ class Controller1D:
         self.u_history = None 
 
 
+    def plotS(self):
+        import matplotlib.pyplot as plt 
+
+        c = 1000/4000
+        x1 = np.linspace(0, 4200, 500)
+        x2 = np.linspace(0, -450, 400)
+        
+        X1,X2 = np.meshgrid(x1*c, x2*c)
+        
+        S1 = X1 + (4+self.rho+self.eps)/(2*self.rho+2*self.eps)*X2*np.abs(X2)
+        S2 = X1 + 1/(2-2*self.alpha)*X2*np.abs(X2)
+        
+        plt.contour(X1/c, X2/c, S1, 0, colors='k', linestyles='dashed')
+        plt.contour(X1/c, X2/c, S2, 0, colors='b', linestyles='dashed')
+
+
 def test():
     """ Recreates the example from the paper: 
         Quasi Time Fuel Optimal Feedback Control 
@@ -100,14 +131,15 @@ def test():
     k = .5 
     mu = 4.887e12
 
-    x0 = [c*4000, c*-120, 1050] # pos vel mass 
+    x0 = [4000, -120, 1050] # pos vel mass 
+
     controller_mfc = Controller1D(rho=1, eps=2, alpha=0.4)
 
     def dyn(x, t,):
         M = x[2]
     
-        u = controller_mfc(x[:-1],)
-        dv = c*(((p+400*np.exp(-0.02*t))/M)*u - mu/(x[0]/c + R)**2)
+        u = controller_mfc(x[:-1]*c,)
+        dv = (((p+400*np.exp(-0.02*t))/M)*u - mu/(x[0] + R)**2)
         
         dM = -k*np.abs(u)
         if np.abs(x[0]) + np.abs(x[1]) < 1:
@@ -123,17 +155,22 @@ def test():
 
     iterm = np.argmin(dx)
 
-    plt.plot(x[:iterm,0]/c, x[:iterm,1]/c)
+    controller_mfc.plotS()
+    plt.plot(x[:iterm, 0], x[:iterm, 1])
     plt.title("Phase Portrait")
+
     plt.figure()
     plt.plot(t[:iterm], controller_mfc.u_history[:iterm])
     plt.xlabel('Time (s)')
+    plt.ylabel("Throttle setting")
+
     plt.figure()
     plt.plot(t[:iterm], x[:iterm, 2])
     plt.xlabel('Time (s)')
-    plt.figure()
-    plt.plot(t[:iterm], x[:iterm, :2])
-    plt.xlabel('Time (s)')
+    plt.ylabel('Mass (kg)')
+    # plt.figure()
+    # plt.plot(t[:iterm], x[:iterm, :2])
+    # plt.xlabel('Time (s)')
     print("Time of flight = {:.2f} s\nFinal altitude = {:.3f} m\nFinal velocity = {:.3f} m/s\nFuel consumed = {:.1f} kg".format(t[iterm],x[-1,0],x[-1,1],x0[2]-x[-1,2]))
     plt.show()
 
