c9f834b2c3e7dffea80298b822d0d6f972d7d24d
cdnoyes <aero.noyes@gmail.com>
2019-03-25 23:32:30 -0700

    adds base class for providing sdc factorizations. Implements a scalar polynomial system using the base class.

---
+++ EntryGuidance/SDC/SDCBase.py
@@ -0,0 +1,51 @@
+"""SDC factorizations of rotational spacecraft dynamics 
+
+    In some papers, C(x) defines the output vector
+    while in others it defines the terminal constraint. 
+
+
+"""
+
+import numpy as np 
+import abc 
+
+
+class SDCBase(abc.ABC):
+    """
+        Base class for SDC factorizations 
+
+    """
+    @property
+    @abc.abstractmethod
+    def n(self):
+        """ State dimension """
+        return 0
+
+    @property
+    @abc.abstractmethod
+    def m(self):
+        """ Control dimension """
+        return 0
+
+    def A(self, t, x):
+        raise NotImplementedError
+
+    def B(self, t, x):
+        raise NotImplementedError
+
+    def C(self, t, x):  
+        raise NotImplementedError
+
+    def D(self, t):
+        """ Affine terms """
+        return np.zeros(self.n)
+
+    def __call__(self, t, x):
+        return self.A(t, x), self.B(t, x), self.C(t, x), self.D(t)
+
+    def dynamics(self, u):
+        def controlled_dynamics(x, t):
+            A, B, _, D = self.__call__(t, x)
+            return A.dot(x) + B.dot(u) + D
+        return controlled_dynamics
+         
---
+++ EntryGuidance/SDC/ScalarSystems.py
@@ -0,0 +1,43 @@
+"""
+    SDC factorizations of a scalar polynomial system 
+
+"""
+
+import numpy as np 
+from SDCBase import SDCBase
+
+
+class PolySystem(SDCBase):
+    """
+        dx = ax + bx^2 + cx^3 + u
+
+    """
+
+    @property
+    def n(self):
+        return 1
+    
+    def m(self):
+        return 1
+
+    def __init__(self, a, b, c):
+        self.abc = (a, b, c)
+
+    def A(self, t, x):
+        a,b,c = self.abc
+        return np.asarray([a+b*x+c*x**2])
+
+    def B(self, t, x):
+        return np.array([[1]])
+
+    def C(self, t, x):  
+        return np.eye(1)
+
+    def D(self, t):
+        """ Affine terms """
+        return np.zeros(1)
+
+
+
+
+
