8a66b9365b7db8b9245dfdeafba237068467916c
Noyes <aero.noyes@gmail.com>
2019-03-27 13:18:18 -0700

    Updated TSDRE to handle a vector of terminal constraints.

--- Utils/TSDRE.py
+++ Utils/TSDRE.py
@@ -7,8 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d 
 
-from RK4 import RK4
-
+try:
+    from RK4 import RK4
+except ModuleNotFoundError:
+    from Utils.RK4 import RK4
 
 class TSDRE:
     """ Takes an SDC-factorized class and solves
@@ -19,7 +21,7 @@ class TSDRE:
         self.cache = None 
 
 
-    def __call__(self, tc, x0, model, problem):
+    def __call__(self, tc, x0, model, problem, integration_steps=20):
         """ 
             Model is an SDC class 
             Problem is a dictionary that has 
@@ -35,7 +37,7 @@ class TSDRE:
         R = problem['R']
         Ri = np.linalg.inv(R)
 
-        t = np.linspace(tc, tf, 20)
+        t = np.linspace(tc, tf, integration_steps)
         tb = t[::-1]
 
         n = model.n 
@@ -43,6 +45,11 @@ class TSDRE:
         # Compute current system matrices 
         A, B, _, D = model(tc, x0)  
         c, C = problem['constraint'](x0)
+        try:
+            p = len(c)
+            V0f = np.zeros((p, p))
+        except TypeError:
+            V0f = [0]
 
         # Compute gain matrices 
         S0 = RK4(dS0, np.zeros((n, n)), tb, args=(A, B, Q, Ri))[::-1]
@@ -51,17 +58,17 @@ class TSDRE:
         P0 = RK4(dP0, C, tb, args=(A, B, Q, Ri, S0i))[::-1]
         P0i = interp1d(t, P0, axis=0, bounds_error=False, fill_value=(P0[0], P0[-1]))
 
-        V0 = RK4(dV0, [0], tb, args=(B, Ri, P0i))[-1]
+        V0 = RK4(dV0, V0f, tb, args=(B, Ri, P0i))[-1]
 
         S1 = RK4(dS1, np.zeros((n,)), tb, args=(A, B, D, Q, Ri, S0i))[::-1]
         S1i = interp1d(t, S1, axis=0, bounds_error=False, fill_value=(S1[0], S1[-1]))
 
-        V1f = c - C.dot(x0)
-        V1 = RK4(dV1, V1f, tb, args=(A, B, D, Q, Ri, P0i, S1i))[-1]
+        V1f = c - C.dot(x0)  # For now, only a single constraint is considered so this is a scalar value 
+        V1 = RK4(dV1, np.atleast_1d(V1f), tb, args=(A, B, D, Q, Ri, P0i, S1i))[-1]
 
         nu = lagrange(x0, P0[0], np.atleast_2d(V0),  V1)
         self.cache = nu 
-        return control(x0, B, Ri, S0[0], S1[0], P0[0], nu)
+        return control(x0, B, Ri, S0[0], S1[0], np.atleast_2d(P0[0]), nu)  # might need to use np.array( ndmin=n) if the scalar case doesn't work this way 
 
     # def solve(self, model, problem):
         # """ Calls __call__ method and integrates the resulting solution till tf """
@@ -167,7 +174,130 @@ def test():
     example([-1, 0, 2, 4], [0,0,0], nl_constraint, 20)  # ex2 from reference with add'l initial conditions
     # example([-1, 0, 4], [1, 0, -0.1], nl_constraint, 30)  
 
+def test2d():
+    import pandas as pd 
+
+    import sys 
+    sys.path.append("./EntryGuidance")
+    from SDC.SDCBase import SDCBase
+    from progress import progress 
+
+    tf = 3
+    N = 30
+    a = 0.1
+    # x0 = [13, -5.5] 
+    # x0_mc = np.random.multivariate_normal(x0[:2], np.diag([1,1]), 20) 
+    n_samples = 50
+    x0_mc = np.random.random((n_samples,2))  # This isn't neighboring OC so we can use an arbitrary range of IC 
+    x0_mc[:,0] = 6 + 6*x0_mc[:,0]
+    x0_mc[:,1] = -5 + 10*x0_mc[:,1]
+
+
+    class TerminalManifold(SDCBase):
+        @property
+        def n(self):
+            """ State dimension """
+            return 2
+
+        @property
+        def m(self):
+            """ Control dimension """
+            return 1
+        
+        def A(self, t, x):
+            return np.array([[0,1],[0, -a*np.sqrt(1e-5+x[1]**2)]])
+
+        def B(self, t, x):
+            return np.array([[0],[1]])
+        
+        def C(self, *args):
+            return np.eye(2)
+        
+    def constraint(x): 
+        # return (x[0] - x[1]**2)**2, np.array([2*x[0]-2*x[1]**2, -4*x[0]*x[1] + 4*x[1]**3]).T
+        # return (x[0] - x[1]**2), np.array([np.ones_like(x[0]), -2*x[1]]).T
+        # return x[0]**2 + x[1]**2 - 10, np.array([2*x[0], 2*x[1]]).T  # This one doesn't work well for some reason, perhaps the lack of linear terms
+        # return x[0]**2 + x[1]**2 + 2*x[0]*x[1]- 10, np.array([2*x[0]+2*x[1], 2*x[0]+2*x[1]]).T  
+        # return x[0] + 0.1*x[0]**2 + x[1] + 0.1*x[1]**2 - 3, np.array([1+0.2*x[0], 1+0.2*x[1]]).T
+        # return x[0], np.array([1, 0]).T
+        # return x[0]-x[1], np.array([1, -1]).T
+        # return np.cos(x[0]), np.array([-np.sin(x[0]), 0]).T  # This one works impressively well 
+        # return np.cos(x[0])+np.sin(x[1]), np.array([-np.sin(x[0]), np.cos(x[1])]).T  # This one works impressively well 
+        # return x[1]*np.cos(x[0])-1, np.array([-np.sin(x[0])*x[1], np.cos(x[0])]).T 
+
+        return x - np.array([1,0]), np.eye(2)  # A 2d terminal constraint!!
+
+    problem = {'tf': tf, 'Q': np.eye(2)*0, "R": [[1]], 'constraint': constraint}
+    model = TerminalManifold()
+    controller = TSDRE()
+
+    t = np.linspace(0, tf, N)  # These are the N-1 times at which control is updated, and tf 
+    dt = np.diff(t)[0]
+    XMC = []
+    Nuf = []
+    for x0 in x0_mc:
+        if progress(len(XMC), n_samples):
+            print("Iter {}".format(len(XMC)))
+        X = [x0]
+        U = []
+        Nu = []
+        tc = 0 
+
+        for i in range(N-1):
+            u = controller(tc, X[-1], model, problem)
+            if 0:
+                B = 6
+                u = np.clip(u, -B, B)  # Optional saturation effects
+            delta = min(dt, t[-1]-tc)
+            xi = RK4(model.dynamics(u), X[-1], np.linspace(tc, tc+delta, 3))  # _ integration steps per control update 
+            X.append(xi[-1])
+            U.append(u)
+            Nu.append(controller.cache)
+            tc += delta
+        plt.figure(1)
+        plt.plot(t[:-1], Nu)
+        plt.xlabel('Time')
+        plt.ylabel('Lagrange Multiplier(s)')
+
+        plt.figure(3)
+        plt.plot(t[:-1], U)
+        plt.xlabel('Time')
+        plt.ylabel('Control')
+        XMC.append(X)
+        Nuf.append(Nu[-1])
+
+    XMC = np.array(XMC)
+    Nuf = np.array(Nuf).squeeze()
+    print(XMC.shape)  # M samples, N points, n states
+    c = (Nuf - Nuf.min())/Nuf.max()
+    # d = {'x' : XMC[:,-1,1].squeeze(), 'y': XMC[:,-1,0].squeeze(), 'nu': Nuf.squeeze()}
+    # df = pd.DataFrame(data=d)
+    plt.figure(2)
+    # df.plot(kind='scatter', x='x', y='y', c='nu')
+    # plt.scatter(XMC[:,0,1], XMC[:,0,0], c=c.squeeze(), label='Initial State')  # Initial
+    plt.scatter(XMC[:,0,1], XMC[:,0,0], c='g', label='Initial State')  # Initial
+    plt.scatter(XMC[:,-1,1], XMC[:,-1,0], marker='o', label="Final State", c='r')  # Final
+    for traj in XMC:
+        plt.plot(traj.T[1], traj.T[0], 'b', alpha=0.2)
+        
+
+    if 0:
+        x = np.linspace(0, 15, 500)
+        y = np.linspace(-10,10, 600)
+        XY = np.meshgrid(x,y)
+        Z = constraint(XY)[0]
+        ctr = plt.contour(XY[1], XY[0], Z, [0], colors='k')
+        h = ctr.legend_elements()[0][0]
+        plt.legend([h], ["Terminal Manifold"])
+    plt.title("Phase Portrait")
+    plt.xlabel('v')
+    plt.ylabel('x')
+    plt.show()
+
+
+
 if __name__ == "__main__":
-    test()
+    # test()
+    test2d()
 
 
