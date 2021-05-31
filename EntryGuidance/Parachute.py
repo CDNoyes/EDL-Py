4f2c68deebdf9ba3d1d90efa45627e7f2ed87580
cdnoyes <aero.noyes@gmail.com>
2021-05-30 22:38:38 -0700

    Some srs updates

--- EntryGuidance/Parachute.py
+++ EntryGuidance/Parachute.py
@@ -3,23 +3,25 @@ import matplotlib.pyplot as plt
 from Planet import Planet
 
 
-def Draw(figure=None, show=False, label=False, figsize=None, fontsize=16):
-    plt.rc('font', family='serif')
+def Draw(figure=None, show=False, label=False, figsize=None, fontsize=16, qlims=[300,800], Mlims=[1.4,2.2]):
+#     plt.rc('font', family='serif')
 
     Mars = Planet(name='Mars', rho0=0, scaleHeight=0, model='exp')
 
     # Dynamic pressure limits
-    q1 = 300
-    q2 = 800
+    q1,q2 = qlims
+#     q1 = 300
+#     q2 = 800
 
     # Mach number limits
-    M1 = 1.4
-    M2 = 2.2
+    M1,M2 = Mlims
+#     M1 = 1.4
+#     M2 = 2.2
 
     # Min altitude, meters, artificial limit based on mission req.
     h_min = 6e3
 
-    h = np.linspace(h_min, 18e3, 500)
+    h = np.linspace(h_min, 20e3, 500)
 
     rho, a = Mars.atmosphere(h)
     Vmax = np.minimum(a*M2, np.sqrt(2*q2/rho))
@@ -40,7 +42,7 @@ def Draw(figure=None, show=False, label=False, figsize=None, fontsize=16):
     else:
         plt.figure(figure, figsize=figsize)
 
-    plt.plot(Vmax, h/1000, 'k', Vmin, h/1000, 'k', x, y, 'k')
+    plt.plot(Vmax, h/1000, 'r', Vmin, h/1000, 'r', x, y, 'r')
     plt.xlabel('Velocity (m/s)', fontsize=fontsize)
     plt.ylabel('Altitude (km)', fontsize=fontsize)
     plt.xticks(fontsize=fontsize)
@@ -51,10 +53,10 @@ def Draw(figure=None, show=False, label=False, figsize=None, fontsize=16):
         ax = plt.gca()
         ax.annotate('h > {}'.format(h_min/1000), xy=(370, 0.25 + h_min/1000), fontsize=fontsize)
 
-        ax.annotate('M > {}'.format(M1), xy=(313, 1.2+h_min/1000), fontsize=fontsize)
-        ax.annotate('M < {}'.format(M2), xy=(445, 5 + h_min/1000), fontsize=fontsize)
+#         ax.annotate(r'M > {}'.format(M1), xy=(313, 1.2+h_min/1000), fontsize=fontsize)
+        ax.annotate('M < {}'.format(M2), xy=(435, 6 + h_min/1000), fontsize=fontsize)
         ax.annotate('q < {}'.format(q2), xy=(440, 1.5 + h_min/1000), fontsize=fontsize)
-        ax.annotate('q > {}'.format(q1), xy=(370, 11.5), fontsize=fontsize)
+        ax.annotate('q > {}'.format(q1), xy=(325, 10.5), fontsize=fontsize)
 
     if show:
         plt.show()
